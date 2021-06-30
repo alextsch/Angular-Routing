3f8ca7b0574223a39fe4d98072b6736d4b65787d
Alexander Tscherwonnych <alexander.tscherwonnych@cgm.com>
2021-06-30 21:25:02 +0200

    Beim Wechsel auf neues Notebook

--- APM-Start/src/app/user/user.module.ts
+++ APM-Start/src/app/user/user.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  { path: 'login', component: LoginComponent }
+]
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     LoginComponent
